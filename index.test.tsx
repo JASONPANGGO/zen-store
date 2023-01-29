4ff76cc03aa7daa1ec225aa06fa32bc7155ee709
JASONPANGGO <jason_pang@126.com>
2023-01-30 01:44:33 +0800

    feat: editorconfig

--- index.test.tsx
+++ index.test.tsx
@@ -1,41 +1,50 @@
-import React, { useSyncExternalStore } from "react";
-import ZenStore from "./index"
+import React, { useSyncExternalStore } from 'react';
+import ZenStore from './index';
 import { fireEvent, render, screen } from '@testing-library/react';
 
 const CountStore = new ZenStore(0);
 
 function Button() {
-    const handleClick = () => {
-        CountStore.update(CountStore.getSnapshot() + 1)
-    }
-    return <button role="button" onClick={handleClick}>add</button>
+  const handleClick = () => {
+    CountStore.update(CountStore.getSnapshot() + 1);
+  };
+  return (
+    <button role="button" onClick={handleClick}>
+      add
+    </button>
+  );
 }
 
 function Counter() {
-    const count = useSyncExternalStore(CountStore.subscribe.bind(CountStore), CountStore.getSnapshot.bind(CountStore));
+  const count = useSyncExternalStore(
+    CountStore.subscribe.bind(CountStore),
+    CountStore.getSnapshot.bind(CountStore)
+  );
 
-    return <div role="counter">
-        <i role="count">{count}</i>
-        <Button />
+  return (
+    <div role="counter">
+      <i role="count">{count}</i>
+      <Button />
     </div>
+  );
 }
 
 describe('<Counter />', () => {
-    it('Counter should be rendered', async ()=>{
-        render(<Counter />);
-        const element = await screen.findByRole('counter');
-        expect(element).toBeTruthy();
-    })
-    
-    it('Button click should be handled to counter action', async () => {
-        render(<Counter />);
-        const button = await screen.findByRole("button");
-        const count = await screen.findByRole("count");
+  it('Counter should be rendered', async () => {
+    render(<Counter />);
+    const element = await screen.findByRole('counter');
+    expect(element).toBeTruthy();
+  });
 
-        expect(count.textContent).toBe("0")
-        
-        fireEvent.click(button);
+  it('Button click should be handled to counter action', async () => {
+    render(<Counter />);
+    const button = await screen.findByRole('button');
+    const count = await screen.findByRole('count');
 
-        expect(count.textContent).toBe("1")
-    })
-})
+    expect(count.textContent).toBe('0');
+
+    fireEvent.click(button);
+
+    expect(count.textContent).toBe('1');
+  });
+});
--- index.ts
+++ index.ts
@@ -2,26 +2,26 @@ type StoreChangeHandler = () => void;
 type UnsubscribeHandler = () => void;
 
 export default class ZenStore {
-    private value: any;
-    private listeners: StoreChangeHandler[] = [];
+  private value: any;
+  private listeners: StoreChangeHandler[] = [];
 
-    constructor(value: any) {
-        this.value = value;
-    }
+  constructor(value: any) {
+    this.value = value;
+  }
 
-    subscribe(handler: StoreChangeHandler): UnsubscribeHandler {
-        this.listeners.push(handler)
-        return () => {
-            this.listeners = this.listeners.filter(l => l !== handler);
-        }
-    }   
-    
-    getSnapshot() {
-        return this.value;
-    }
+  subscribe(handler: StoreChangeHandler): UnsubscribeHandler {
+    this.listeners.push(handler);
+    return () => {
+      this.listeners = this.listeners.filter((l) => l !== handler);
+    };
+  }
 
-    update(newValue: any) {
-        this.value = newValue;
-        this.listeners.forEach(handler => handler());
-    }
+  getSnapshot() {
+    return this.value;
+  }
+
+  update(newValue: any) {
+    this.value = newValue;
+    this.listeners.forEach((handler) => handler());
+  }
 }
